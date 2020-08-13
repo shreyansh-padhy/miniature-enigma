37d3a745ee310f7c4fbe1931566b80f284e58089
shreyansh-padhy <66857223+shreyansh-padhy@users.noreply.github.com>
2020-08-12 18:02:09 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas, backGroundImage;
+var database;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+
+function setup(){
+  database = firebase.database();
+  canvas = createCanvas(500,500);
+
+   game = new Game();
+   game.getState();
+   game.start();
+}
+
+function draw(){
+ 
+}
+
+ 
